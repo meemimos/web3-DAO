714ebf95f89c4382354dfbf19170ee3fdba55cea
meemimos <sayabcz@hotmail.com>
2022-07-19 20:02:50 +1000

    Static Proposal page

--- src/sampleVotes.js
+++ src/sampleVotes.js
@@ -1,22 +1,26 @@
-[
-    [
-      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
-      <Icon fill="#268c41" size={24} svg="checkmark" />,
-    ],
-    [
-      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
-      <Icon fill="#268c41" size={24} svg="checkmark" />,
-    ],
-    [
-      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
-      <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
-    ],
-    [
-      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
-      <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
-    ],
-    [
-      "0x4d2044D8D568c1644158625930De62c4AbBB004a",
-      <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
-    ],
-  ]
+import { Icon } from "web3uikit";
+
+const sampleVotes = [
+  [
+    "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+    <Icon fill="#268c41" size={24} svg="checkmark" />,
+  ],
+  [
+    "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+    <Icon fill="#268c41" size={24} svg="checkmark" />,
+  ],
+  [
+    "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+    <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
+  ],
+  [
+    "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+    <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
+  ],
+  [
+    "0x4d2044D8D568c1644158625930De62c4AbBB004a",
+    <Icon fill="#d93d3d" size={24} svg="arrowCircleDown" />,
+  ],
+]
+
+export default sampleVotes;
